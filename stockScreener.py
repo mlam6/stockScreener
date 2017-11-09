c988bcbfe91ffb97e638c8bb7a719310880aa4cf
Michelle L <michelle.lam307@gmail.com>
2017-11-09 09:21:57 -0500

    Volume done. Fixed performance by using threads

--- stockScreener.py
+++ stockScreener.py
@@ -1,44 +1,99 @@
 from alpha_vantage.techindicators import TechIndicators
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.sectorperformance import SectorPerformances
+import time
+import thread, threading
+import Queue
 
-# Store NASDAQ file into array and sort list
-file = open("input/NASDAQ2.txt", "r")
+
+class Worker(threading.Thread):
+   def __init__(self, threadID, name):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.isRunning = False
+      self.symbol = None
+   def run(self):
+      self.isRunning = True
+      #print ("Starting",  self.name, "Processing", self.symbol)
+      getVol(self.symbol)
+      #print ("Exiting", self.name, "Processed", self.symbol)
+      self.isRunning = False
+
+
+
+# Sort and remove whitespace in NASDAQ file
+file = open("input/NASDAQ.txt", "r")
 text = file.read()
-listOfSymbols = text.split("\r\n")
+listOfSymbols = text.split("\n")
 listOfSymbols.sort()
+listOfSymbols = map(lambda x: x.strip(), listOfSymbols)
 
-# Final list of symbols
-finalList = []
+# Store symbols that passed tests
+finalList = Queue.Queue()
+garbage = Queue.Queue()
+
+# Split a list 
+def split(arr, size):
+    arrs = []
+    while len(arr) > size:
+        pice = arr[:size]
+        arrs.append(pice)
+        arr = arr[size:]
+    arrs.append(arr)
+
+    return arrs
 
 
 # If volume is > 250,000
-def getVol():
-    for symbol in listOfSymbols:
-        ts=TimeSeries(key='GSD3E3P11LSBZG5O', output_format='pandas')
-        try:
-            data, meta_data = ts.get_intraday(symbol)
-        except Exception:
-            continue
- 
-        if (data.volume == 250000).all:
-            finalList.append(symbol)
+def getVol(sym):
+    global finalList
+    ts=TimeSeries(key='GSD3E3P11LSBZG5O', output_format='pandas')
+    
+    try:
+        data, meta_data = ts.get_intraday(symbol = sym)
+    except Exception:
+        return
 
-    # Testing 
-    print finalList
+    if ((data["volume"].iloc[-1]) > 250000).all():   
+        finalList.put(sym)
+        #print sym 
+    else:
+        garbage.put(sym)      
+
+
+# Threads created to process all the symbols
+def getVolAll():
+    global finalList
+
+    nThreads = 100
+    threadPool = [None] * nThreads
+    for i in range(nThreads):
+        threadPool[i] = Worker(i, i)
+    threadIndex = 0
+
+    for symbol in listOfSymbols:
+        if threadPool[threadIndex].isRunning:
+            threadPool[threadIndex].join()
+
+        threadPool[threadIndex] = Worker(threadIndex, threadIndex)
+        threadPool[threadIndex].symbol = symbol
+        threadPool[threadIndex].start()
+        threadIndex += 1
+        if threadIndex == nThreads:
+            threadIndex = 0
+
+    for thrd in threadPool:
+        thrd.join()
+
+    finalList = list(finalList.queue)
+    print ("Final List:")
+    for item in finalList:
+        print item
 
 
 # If SMA is > quote
-def getSMA():
-    for symbol in finalList:
-        ti = TechIndicators(key='GSD3E3P11LSBZG5O', output_format='pandas')
-        data, meta_data = ti.get_sma(symbol)
-
-        if(data.SMA > data.close):
-            finalList.append(symbol)
-
-        # Testing
-        print finalList
+#def getSMA():
 
 
 # If MACD is > 0
@@ -51,14 +106,19 @@ def getSMA():
 
 # Return list if symbol passes Vol, SMA, MACD, and Stochastic
 #def returnList():
-   #print finalList 
 
 
 # returnList()
 
-# TESTING
-# ------------------
-getVol()
+''' TESTING
+------------------'''
+#getVol()
+getVolAll()
 #getSMA()
 #getMACD()
 #getSto()
+#split(listOfSymbols, 4)
+
+
+
+
